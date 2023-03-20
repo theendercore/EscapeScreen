74d497124eba44a482973699865f9f1bbbf6c8d3
theendercore <theendercore@gmail.com>
2023-03-21 00:51:55 +0200

    made the esc menu functional

---
+++ src/main/java/com/theendercore/escapescreen/mixins/KeyboardMixin.java
@@ -0,0 +1,38 @@
+package com.theendercore.escapescreen.mixins;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static com.theendercore.escapescreen.EscapeScreenClient.newEscKey;
+
+@Environment(EnvType.CLIENT)
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+
+    @Final
+    @Shadow
+    private MinecraftClient client;
+
+    @Inject(method = "onKey", at = @At("HEAD"))
+    public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        if (window == this.client.getWindow().getHandle()) {
+            if (this.client.currentScreen == null || this.client.currentScreen.passEvents) {
+                if (this.client.currentScreen == null) {
+                    if (newEscKey.wasPressed()) {
+                        boolean bl3 = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 292);
+                        this.client.openPauseMenu(bl3);
+                    }
+                }
+            }
+        }
+    }
+}
